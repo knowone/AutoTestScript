8cb380676f7641863f5f68f36704505c01f7e593
knowone <knowone.omer@gmail.com>
2017-06-29 23:33:29 +0300

    Made some documentations. Added a new For syntax. Testing it...

--- test.sh
+++ test.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
-############################################
-#Change this to the current assignment name:
-SRC_NAME=src
-SOL_NAME=sol
-TESTS_NAME=tests
-BIN_NAME=out
-TEST_OUTPUT=output
-############################################
+#####################################################################################
+ 
+## Replace .. with path to all source directories. Leave as .. if this testing script 
+## folder is in the same folder as the source folders
+SRC_NAME=.. 
 
-SRC_FDR=./$SRC_NAME
+## Name of folder with the school solutions:
+SOL_NAME=sol
+
+## Name of folder with all the tests:
+TESTS_NAME=tests
+
+
+############################################
+TEST_OUTPUT=output
+BIN_NAME=out
+
+SRC_FDR=SRC_NAME
 SOL_FDR=./$SOL_NAME
 TESTS_FDR=./$TESTS_NAME
 
@@ -23,34 +31,39 @@ mkdir $TEST_OUTPUT_FDR
 
 for PROGNAME in $@ 
 do
-  SRC=$SRC_FDR/$PROGNAME.cc
-  SOL=$SOL_FDR/$PROGNAME
+  SRC=$SRC_FDR/$PROGNAME/$PROGNAME.cc
+  SOL=$SOL_FDR/$PROGNAME ##this is the name of the school solution binary.
+			 # improve: Get from user parameters
   MY_SOL=$BIN_FDR/$PROGNAME
   TEST=$TESTS_FDR
   TEST_RES=$TEST_OUTPUT_FDR
   COMPILE= g++ -Wall $SRC -o $MY_SOL
-  TNAME=_test0
+  TNAME=_test0	##test file names are in the form ${PROGNAME}_test0$#
 
   $COMPILE
 
 
 ## Add more tests by changing number from 9 to whatever ##
+## Improve: get num of tests from user. Default is 9
   echo
   echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Testing '$PROGNAME' ~~~~~~~~~~~~~~~~~~~~~~~~~~~~' 
   echo
   echo '~~~~~~ Running tests: ~~~~~~'
-  for i in {0..9}
+  for i in 0 9
 
 ## ---------------------------------------------------- ##
   do
-    if [ -f $TESTS_FDR/$PROGNAME$TNAME$i.in ]; then
+    if [ -f $TESTS_FDR/$PROGNAME$TNAME$i.in ]; then ##If the test exists do:
       echo 'Running Test0'$i':'
-      $MY_SOL <$TESTS_FDR/$PROGNAME$TNAME$i.in >$TEST_RES/res0$i.out
-      $SOL <$TESTS_FDR/$PROGNAME$TNAME$i.in >$TEST_RES/ressol0$i.out
+      $MY_SOL <$TESTS_FDR/$PROGNAME$TNAME$i.in >$TEST_RES/res0$i.out	##Run my program
+      $SOL <$TESTS_FDR/$PROGNAME$TNAME$i.in >$TEST_RES/ressol0$i.out	##Run solution
 
       if [[ $(diff $TEST_RES/res0$i.out $TEST_RES/ressol0$i.out | head -c1 | wc -c) -ne 0 ]]; then
-	FLAG=1        
+	## If diff returned any result it means it found a difference:
+	FLAG=1        ## Flag to not delete the test output
 	echo 'Test Result:'
+
+	##print the result:
         echo 'Diff '$TNAME$i '('res0$i.out, ressol0$i.out'):'
         diff $TEST_RES/res0$i.out $TEST_RES/ressol0$i.out
       else
@@ -66,7 +79,7 @@ do
 
 ## Add more tests by changing number from 9 to whatever ##
 
-  for i in {0..9}
+  for i in 0 9
 
 ## ---------------------------------------------------- ##
   do
