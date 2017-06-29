ba82fd8bc77b25c2adda99f929b495d6d4495e4c
knowone <knowone.omer@gmail.com>
2017-06-30 00:40:28 +0300

    Change cosmetics in display. More documenting. Added a few options for users. Deleted all tests from tests folder

--- test.sh
+++ test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-#####################################################################################
- 
-## Replace .. with path to all source directories. Leave as .. if this testing script 
-## folder is in the same folder as the source folders
-SRC_NAME=.. 
+SRC_NAME=..
+######################  USER MODIFICATIONS HERE  ##########################
+  
+## Replace .. with path to all source directories.
+#Uncomment next line if src files are in AutoTestScript/src/
+#SRC_NAME=src
 
 ## Name of folder with the school solutions:
 SOL_NAME=sol
@@ -13,7 +14,13 @@ TESTS_NAME=tests
 
 ## Highest number of tests available
 MAX_TEST=9
-############################################
+
+## If school solution has a postfix after the assignment name i.e. ex4asol
+## where sol is a postfix, add that postfix between the ''
+## Example: SOL_ADD='sol' (for ex4asol)
+SOL_ADD=''
+
+######################  END USER MODIFICATIONS  ##########################
 TEST_OUTPUT=output
 BIN_NAME=out
 
@@ -33,7 +40,7 @@ mkdir $TEST_OUTPUT_FDR
 for PROGNAME in $@ 
 do
   SRC=$SRC_FDR/$PROGNAME/$PROGNAME.cc
-  SOL=$SOL_FDR/$PROGNAME ##this is the name of the school solution binary.
+  SOL=$SOL_FDR/$PROGNAME$SOL_ADD ##this is the name of the school solution binary.
 			 # improve: Get from user parameters
   MY_SOL=$BIN_FDR/$PROGNAME
   TEST=$TESTS_FDR
@@ -43,14 +50,50 @@ do
   TNAME= ##test file names are in the form ${PROGNAME}_test0$#
 
   $COMPILE
+  echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+  echo
+  echo
+  echo '~~~~~~~~~~~~~~~~~~~~~ Running Valgrind for '$PROGNAME '~~~~~~~~~~~~~~~~~~~~~'
+  echo
+  echo "     ~~~~~~~~~~~~~~~~~~~~   Running Valgrind:   ~~~~~~~~~~~~~~~~~~~~"
 
+## Add more tests by changing number from 9 to whatever ##
+
+  i=0
+  while [ $i -le $MAX_TEST ]
+
+## ---------------------------------------------------- ##
+  do
+    if [ $i -le 9 ]; then	
+	TNAME='_test0'
+    else			##Case where # of tests > 9
+	TNAME='_test'
+    fi
+	## Test file name exists in the output directory, so run valgrind on it
+    if [[ $(ls $TESTS_NAME | grep $PROGNAME$TNAME$i.in | head -c1 | wc -c) -ne 0 ]]; then
+   
+      valgrind $MY_SOL <$TESTS_FDR/$PROGNAME$TNAME$i.in >$TEST_RES/valgrind_$PROGNAME'_'$i.out 2>&1
+  
+    fi
+##Search for SUMMARY in valgrind ouput, if exists and valgrind was perfomred (head of file is not empty)
+    if [[ $(grep -s -A 1 'SUMMARY:' $TEST_RES/valgrind_$PROGNAME'_'$i.out | head -c1 | wc -c) -ne 0 ]]; then  
+      echo 
+      echo valgrind$i.out':'
+      echo 	
+      grep -s -A 1 'SUMMARY:' $TEST_RES/valgrind_$PROGNAME'_'$i.out
+    fi
+  i=$(( i+1 ))
+  done
+ echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+done
 
 ## Add more tests by changing number from 9 to whatever ##
 ## Improve: get num of tests from user. Default is 9
   echo
-  echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Testing '$PROGNAME' ~~~~~~~~~~~~~~~~~~~~~~~~~~~~' 
+  echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Testing Program '$PROGNAME' ~~~~~~~~~~~~~~~~~~~~~~~~~~~~' 
   echo
-  echo '~~~~~~ Running tests: ~~~~~~'
+  echo '         ~~~~~~~~~~~~~~~~~~~~   Running Tests:   ~~~~~~~~~~~~~~~~~~~~'
+  echo
   
   i=0
   while [ $i -le $MAX_TEST ]
@@ -82,39 +125,7 @@ do
   i=$(( i+1 ))
   done
  
-  echo '~~~~~~~~~~~~~~~~~~~~~ Running Valgrind for '$PROGNAME '~~~~~~~~~~~~~~~~~~~~~'
-  echo
-  echo "~~~~~ Running Valgrind ~~~~~"
-
-## Add more tests by changing number from 9 to whatever ##
-
-  i=0
-  while [ $i -le $MAX_TEST ]
-
-## ---------------------------------------------------- ##
-  do
-    if [ $i -le 9 ]; then	
-	TNAME='_test0'
-    else			##Case where # of tests > 9
-	TNAME='_test'
-    fi
-	## Test file name exists in the output directory, so run valgrind on it
-    if [[ $(ls $TESTS_NAME | grep $PROGNAME$TNAME$i.in | head -c1 | wc -c) -ne 0 ]]; then
-   
-      valgrind $MY_SOL <$TESTS_FDR/$PROGNAME$TNAME$i.in >$TEST_RES/valgrind_$PROGNAME'_'$i.out 2>&1
-  
-    fi
-##Search for SUMMARY in valgrind ouput, if exists and valgrind was perfomred (head of file is not empty)
-    if [[ $(grep -s -A 1 'SUMMARY:' $TEST_RES/valgrind_$PROGNAME'_'$i.out | head -c1 | wc -c) -ne 0 ]]; then  
-      echo 
-      echo valgrind$i.out':'
-      echo 	
-      grep -s -A 1 'SUMMARY:' $TEST_RES/valgrind_$PROGNAME'_'$i.out
-    fi
-  i=$(( i+1 ))
-  done
- echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
-done
+ echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 
 #delete output folder if No issues in any test
 rm -rf $BIN_FDR
@@ -123,8 +134,9 @@ if [ $FLAG -eq 0 ]; then
   echo 'All tests completed successfuly. Deleting output folder...'
   rm -rf $TEST_RES  
 else
-  echo
-  echo 'See results in '$TEST_OUTPUT 'folder'
+  echo '* Some tests failed :( Scroll up to see the tests differences.'
+  echo '** See full test results in '$TEST_OUTPUT 'folder'...
 fi
-echo "Finished"
+echo "Finished!"
+
 
